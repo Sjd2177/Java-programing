6c8ef291e0c3abdb3d8a0b7a40263bdd1f583e92
Sjd2177 <1480258051@qq.com>
2024-09-21 12:54:48 +0800

    Add files via upload

---
+++ public class q1{.java
@@ -0,0 +1,6 @@
+public class q1{
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java,Welcome to Computer sience");
+        System.out.println("Programming is fun");
+    }
+}
